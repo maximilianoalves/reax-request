7718a1089a8f8b0ab215e1c92fed6d6e9d064d0c
Héliton Nordt <hnordt@hnordt.com>
2016-01-11 18:53:12 -0200

    Fix

--- index.babel.js
+++ index.babel.js
@@ -1,7 +1,7 @@
 import Promise from 'promise';
 import superagent from 'superagent';
 
-const request = (method, url, data, headers) => {
+const request = (method, url, data, headers = {}) => {
   return new Promise((resolve, reject) => {
     superagent(method, url)[method === 'GET' ? 'query' : 'send'](data).set(headers).end((error, response) => {
       if (error) {
