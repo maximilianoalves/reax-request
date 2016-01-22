3d0ca3fd2459eafd9f3567b92ec8f8ed5ea23821
Héliton Nordt <hnordt@hnordt.com>
2016-01-22 16:21:01 -0200

    Add params

--- index.babel.js
+++ index.babel.js
@@ -1,8 +1,9 @@
 import Promise from 'promise';
 import superagent from 'superagent';
 
-const request = (method, url, data, headers) => {
+const request = (params) => {
   return new Promise((resolve, reject) => {
+    const { method, url, data, headers } = params;
     superagent(method, url)[method === 'GET' ? 'query' : 'send'](data).set(headers || {}).end((error, response) => {
       if (error) {
         reject(error.response.body);
